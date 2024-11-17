7162ebd1c27dba9ae9622eed8560eca618527605
綾坂こと <148830788+AXT-AyaKoto@users.noreply.github.com>
2024-11-17 23:45:36 +0900

    デコ左右反転横ずらし 左右反転しなかったときにeasingが左右反転するのはまずい

--- editor/OffsetDecos.js
+++ editor/OffsetDecos.js
@@ -7,6 +7,6 @@ let a = +prompt("傾き", -1), b = +prompt("切片", 0), p = (s, d) => s.padStar
 ${p("", 12)}"color" : ${JSON.stringify(n.color).replace(/,/g, ", ")},
 ${p("", 12)}"start" : { "where": [${x(n.start.where[0], 2, 5)},  ${x(n.start.where[1], 2, 5)}], "when": [${x(n.start.when[0], 0, 3)}, ${x(n.start.when[1], 0, 2)}, ${x(n.start.when[2], 0, 2)}], },
 ${p("", 12)}"end"   : { "where": [${x(n.end.where[0], 2, 5)},  ${x(n.end.where[1], 2, 5)}], "when": [${x(n.end.when[0], 0, 3)}, ${x(n.end.when[1], 0, 2)}, ${x(n.end.when[2], 0, 2)}], },
-${p("", 12)}"easing": { "left": "${n.easing.right}", "right": "${n.easing.left}", }, "speed": ${x(n.speed, 2, 1)},
+${p("", 12)}"easing": { "left": "${n.easing[a >= 0 ? 'left' : 'right']}", "right": "${n.easing[a < 0 ? 'left' : 'right']}", }, "speed": ${x(n.speed, 2, 1)},
         },
 `).join("") + `\n]`;
